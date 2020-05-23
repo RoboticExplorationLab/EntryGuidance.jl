5faae171c24efd2d4f9e5e556669eddc5ecebe62
Zac Manchester <zacm@stanford.edu>
2020-05-22 19:01:56 -0700

    better msl match

--- test/msl_test.jl
+++ test/msl_test.jl
@@ -13,7 +13,7 @@ end
 #https://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/20130010129.pdf
 model = CartesianMSLModel()
 Rm = model.planet.R
-r0 = [Rm+125.0, -631.979, 0.0] #Atmospheric interface at 125 km altitude, 631.979 km down range and 7.869 km cross-range of target
+r0 = [Rm+125.0, 0.0, 0.0] #Atmospheric interface at 125 km altitude, 631.979 km down range and 7.869 km cross-range of target
 V0 = 5.845*3600 #Mars-relative velocity at interface of 5.845 km/sec
 γ0 = -15.474*(pi/180.0) #Flight path angle at interface
 v0 = V0*[sin(γ0), cos(γ0), 0.0]
@@ -27,15 +27,14 @@ tspan = (0.0, 0.1)
 prob = ODEProblem(open_loop_dynamics!,x0,tspan,params)
 soln = solve(prob, Tsit5(), reltol=1e-9, abstol=1e-9)
 
-# using Plots
+using Plots
 # plotly()
 # gr()
-#
-# nsteps = 100
-# t = LinRange(soln.t[1],soln.t[end],nsteps)
-# alt = zeros(nsteps)
-# for k = 1:nsteps
-#     alt[k] = norm(soln(t[k])[1:3])-Rm
-# end
-# plot(t.*60,alt)
-# plot(soln,vars=(1,2))
+
+nsteps = 100
+t = LinRange(soln.t[1],soln.t[end],nsteps)
+alt = zeros(nsteps)
+for k = 1:nsteps
+    alt[k] = norm(soln(t[k])[1:3])-Rm
+end
+plot(t.*60,alt)
