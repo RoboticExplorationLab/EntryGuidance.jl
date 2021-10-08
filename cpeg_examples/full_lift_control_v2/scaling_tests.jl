ae7bd188e1bfb69f19e6670104e895667a6f50bb
Kevin Tracy <ktracy@stanford.edu>
2021-10-08 11:53:42 -0400

    scaled version working

---
+++ cpeg_examples/full_lift_control_v2/scaling_tests.jl
@@ -0,0 +1,105 @@
+using LinearAlgebra
+using EntryGuidance
+const EG = EntryGuidance
+using StaticArrays
+using Attitude
+using MATLAB
+using Infiltrator
+using ForwardDiff
+using Convex
+using Mosek, MosekTools
+# using COSMO
+using SuiteSparse
+using SparseArrays
+# using Interpolations
+
+
+include(joinpath(@__DIR__,"dynamics.jl"))
+include(joinpath(@__DIR__,"rollout_stuff.jl"))
+include(joinpath(@__DIR__,"mpc.jl"))
+include(joinpath(@__DIR__,"post_process.jl"))
+
+
+function dist_from_target(X,xf)
+    rr = normalize(xf[1:3])
+    Qn = I - rr*rr'
+    return norm(Qn*(X[end][1:3]-xf[1:3]))
+end
+
+function first_test()
+
+# dscale = 1.0e3
+# tscale = 1.0
+# uscale = 1.0
+
+model = EntryVehicle(CartesianMSLModel(),1.0)
+#Initial conditions for MSL
+Rm = model.evmodel.planet.R
+r0 = [Rm+125.0, 0.0, 0.0] #Atmospheric interface at 125 km altitude
+V0 = 5.845*3600 #Mars-relative velocity at interface of 5.845 km/sec
+γ0 = -15.474*(pi/180.0) #Flight path angle at interface
+v0 = V0*[sin(γ0), cos(γ0), 0.0]
+x0 = [r0;v0]
+
+#Final conditions for MSL 631.979 km down range and 7.869 km cross-range from entry point
+Rf = Rm+10.0 #Parachute opens at 10 km altitude
+rf = Rf*[cos(7.869/Rf)*cos(631.979/Rf); cos(7.869/Rf)*sin(631.979/Rf); sin(7.869/Rf)]
+vf = zeros(3)
+xf = [rf;vf]
+
+# first rollout to 10km altitude
+dt = 2/3600
+N = 180
+X = NaN*[@SArray zeros(6) for i = 1:N]
+U = [[0.03;0.5] for i = 1:N-1]
+
+T = 7
+althist = [zeros(2) for i = 1:T]
+drhist = [zeros(2) for i = 1:T]
+crhist = [zeros(2) for i = 1:T]
+dunorm = zeros(T)
+
+
+X, U, t_vec, t_impact = rollout(model,deepcopy(x0),U,dt)
+
+
+rnorm = [norm(X[i][1:3]) for i = 1:length(X)]
+vnorm = [norm(X[i][4:6]) for i = 1:length(X)]
+
+Xs = deepcopy(X);
+dscale = 1e3
+tscale = 1
+for i = 1:length(X)
+    r = X[i][1:3]
+    v = X[i][4:6]
+
+    rs = r/dscale
+    vs = v/(dscale/tscale)
+
+    Xs[i] = [rs;vs]
+end
+
+rsnorm = [norm(Xs[i][1:3]) for i = 1:length(Xs)]
+vsnorm = [norm(Xs[i][4:6]) for i = 1:length(Xs)]
+
+
+mat"
+figure
+hold on
+title('scaled')
+plot($rsnorm)
+plot($vsnorm)
+"
+
+mat"
+figure
+hold on
+title('unscaled')
+plot($rnorm)
+plot($vnorm)
+"
+
+return nothing
+end
+
+first_test()
---
+++ cpeg_examples/full_lift_control_v20/rollout_stuff.jl
@@ -0,0 +1,38 @@
+
+function rollout(model,x0,U_in,dt)
+    N = 1000
+    X = [@SVector zeros(6) for i = 1:N]
+    U = [@SVector zeros(2) for i = 1:N-1]
+    X[1] = copy(x0)
+    end_idx = NaN
+    t_vec = 0:dt:((N-1)*dt)
+    for i = 1:(length(X)-1)
+
+        if i > length(U_in)
+            U[i] = deepcopy(U_in[end])
+            # @info "went past planned controls"
+        else
+            U[i] = deepcopy(U_in[i])
+        end
+
+        # step forward in sim
+        X[i+1] = rk4(model,X[i],U[i],dt)
+
+        # check for hitting the 10,000ft mark
+        if altitude(model,X[i+1][1:3]*model.dscale) < 10
+            end_idx = i+1
+            break
+        end
+    end
+    # trim relavent
+    if isnan(end_idx)
+        error("didn't hit the altitude during the rollout")
+    end
+
+    X = X[1:end_idx]
+    U = U[1:(end_idx-1)]
+    t_vec = t_vec[1:end_idx]
+
+    t_impact = 0
+    return X, U, t_vec, t_impact
+end
