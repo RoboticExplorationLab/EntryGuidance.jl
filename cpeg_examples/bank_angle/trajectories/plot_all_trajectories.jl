18188894f029d5a86f3d2887c0417557cae346df
Kevin Tracy <ktracy@stanford.edu>
2021-10-08 20:16:47 -0400

    plotted all the controls

--- cpeg_examples/bank_angle/trajectories/plot_all_trajectories.jl
+++ cpeg_examples/bank_angle/trajectories/plot_all_trajectories.jl
@@ -13,7 +13,6 @@ althist = [l1_traj.alt, l2_traj.alt, fl_traj.alt]
 xf_dr = l1_traj.xf_dr
 xf_cr = l1_traj.xf_cr
 
-
 mat"
 figure
 hold on
@@ -61,6 +60,43 @@ addpath('/Users/kevintracy/devel/WiggleSat/matlab2tikz-master/src')
 matlab2tikz(strcat('cpeg_examples/bank_angle/tikz/all_altdr.tex'))
 %matlab2tikz('bankaoa_alt.tex')
 %close all
+"
+
+# plot controls stuff
+l1b = l1_traj.bank
+l1t = l1_traj.T_vec
+l2b = l2_traj.bank
+l2t = l2_traj.T_vec
+mat"
+figure
+hold on
+plot($l1t(1:end-1)/60, rad2deg($l1b),'linewidth',3)
+plot($l2t(1:end-1)/60, rad2deg($l2b),'linewidth',3)
+legend('L1 cost','L2 cost')
+ylabel('bank angle (deg)')
+xlabel('time (min)')
+addpath('/Users/kevintracy/devel/WiggleSat/matlab2tikz-master/src')
+matlab2tikz(strcat('cpeg_examples/bank_angle/tikz/banks.tex'))
+"
+
+flb = fl_traj.bank
+fla = fl_traj.AoA
+flt = fl_traj.T_vec
+
+@show size(flb)
+@show size(fla)
+@show size(flt)
+
+mat"
+figure
+hold on
+plot($flt(1:end-1)/60, rad2deg($flb),'linewidth',3)
+plot($flt(1:end-1)/60, rad2deg($fla),'linewidth',3)
+legend('bank angle','angle of attack','location','northwest')
+ylabel('aero angle (deg)')
+xlabel('time (min)')
+addpath('/Users/kevintracy/devel/WiggleSat/matlab2tikz-master/src')
+matlab2tikz(strcat('cpeg_examples/bank_angle/tikz/fl_controls.tex'))
 "
 
 end
